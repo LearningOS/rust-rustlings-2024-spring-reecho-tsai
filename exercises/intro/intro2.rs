7ef3fd537ceba86f28223774f64b1c8d91ff18a8
Cai Runpei <cairunpeimail@gmail.com>
2024-04-10 06:20:11 +0000

    update

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -5,8 +5,7 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    println!("Hello {}!");
+    let i:i32 = 233;
+    println!("Hello {:x}!", i);
 }
