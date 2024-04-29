7b0afb72a685257373755e0796481bef42301d27
Cai Runpei <cairunpeimail@gmail.com>
2024-04-29 08:47:34 +0000

    update

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,7 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -19,7 +18,7 @@ where
 
 impl<T> Heap<T>
 where
-    T: Default + std::cmp::PartialOrd + Copy,
+    T: Default,
 {
     pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
         Self {
@@ -40,17 +39,8 @@ where
     pub fn add(&mut self, value: T) {
         //TODO
         self.items.push(value);
-        let mut idx = self.items.len() - 1;
-        
-        while (self.comparator)(&self.items[self.parent_idx(idx)], &self.items[idx]) {
-            // swap(&mut self.items[self.parent_idx(idx)], &mut self.items[idx]);
-            let tmp = self.items[self.parent_idx(idx)];
-            let parent = self.parent_idx(idx);
-            self.items[parent] = value;
-            self.items[idx] = tmp;
-            idx /= 2;
-        } 
-
+        self.count += 1;
+        self.heapify_up(self.count);
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -73,14 +63,41 @@ where
         //TODO
 		let left = self.left_child_idx(idx);
         let right = self.right_child_idx(idx);
-        
-        if self.items[left] > self.items[right] {
-            right
-        } else {
+
+        if right > self.count {
             left
-        }
-        
+        } else if (self.comparator)(&self.items[left], &self.items[right]) {  // ?
+            left
+        } else {
+            right
     }
+
+}
+    fn heapify_up(&mut self, mut idx: usize) {
+        while idx > 1 {
+            let parent_idx = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                self.items.swap(idx, parent_idx);
+                idx = parent_idx;
+            } else {
+                break;
+            }
+        } 
+    }
+
+    fn heapify_down(&mut self, mut idx: usize) {
+        while self.children_present(idx) {
+            let smallest_child_idx = self.smallest_child_idx(idx);
+
+            if (self.comparator)(&self.items[smallest_child_idx], &self.items[idx]) {
+                self.items.swap(smallest_child_idx, idx);
+                idx = smallest_child_idx;
+            } else {
+                break;
+            }
+        }
+    }
+    
 }
 
 impl<T> Heap<T>
@@ -106,7 +123,13 @@ where
 
     fn next(&mut self) -> Option<T> {
         //TODO
-		None
+		if self.count == 0 {
+            return None;
+        }
+        let result = self.items.swap_remove(1);
+        self.count -= 1;
+        self.heapify_down(1); // ?
+        Some(result)
     }
 }
 
